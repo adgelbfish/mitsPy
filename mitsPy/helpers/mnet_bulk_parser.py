02915a162048406d3a2aa016eed87e3e386395d1
Avraham David Gelbfish <adg@adgelb.fish>
2016-10-09 16:43:15 -0400

    took out debugging code

--- mitsPy/helpers/mnet_bulk_parser.py
+++ mitsPy/helpers/mnet_bulk_parser.py
@@ -4,5 +4,4 @@ class MnetBulkParser:
         self.bulk_string = bulk_string
 
     def get_current_temp_c(self):
-        print(int('0x' + self.bulk_string[12] + self.bulk_string[13], 16)/10)
         return int('0x' + self.bulk_string[12] + self.bulk_string[13], 16)/10
